62d3ad3f2321b6120e01fbf5c8902d0db1a05682
Mario90 <mgpinan1990@gmail.com>
2019-06-21 21:17:34 +0100

    Calculator made by if statements

---
+++ calculatornoif.rb
@@ -0,0 +1,13 @@
+def arithmetic(a, b, operators)
+  #your code here
+  for 'add' in operators do (a + b)
+    end
+  for 'subtract' in operators do (a - b)
+    end
+  for 'divide' in operators do (a % b)
+    end
+  for 'multiply' in operators do (a * b)
+    end
+end
+
+arithmetic(6, 2, 'add')
